7161e7b19fbe9ff0013c0f44531789ae028b7d60
nheise <nheise@heise-media.de>
2012-05-17 22:26:09 +0200

    add aspect before

---
+++ test/aspectBeforeTest.js
@@ -0,0 +1,38 @@
+var assert = require('assert');
+
+var aspect = require('../lib/index.js');
+
+var check = {
+  arg1 : 'arg1',
+  arg2 : 'arg2',
+  returnValue : 'return Value',
+  callOriginal : false,
+  callAspect : false 
+};
+
+function A() {
+   this.methodA = function( arg1, arg2 ) {
+      assert.equal( check.arg1, arg1, 'arg1 in original method' );
+      assert.equal( check.arg2, arg2, 'arg2 in original method' );
+      assert.ok( !check.callOriginal, 'original was not invoked' );
+      assert.ok( check.callAspect, 'aspect was invoked' );
+      check.callOriginal = true;
+      return check.returnValue;
+   };
+}
+
+var a = new A();
+
+aspect.before( a, "methodA", function( args ) {
+   assert.equal( args.length, 2 );
+   assert.equal( check.arg1, args[0], 'arg1 in aspect  method' );
+   assert.equal( check.arg2, args[1], 'arg2 in aspect  method' );
+   assert.ok( !check.callOriginal, 'original was not invoked' );
+   assert.ok( !check.callAspect, 'aspect was not invoked' );
+   check.callAspect = true;
+} );
+
+assert.equal( check.returnValue, a.methodA( 'arg1', 'arg2' ), 'original return value must equal' );
+assert.ok( check.callOriginal, 'original was invoked' );
+assert.ok( check.callAspect, 'aspect was invoked' );
+console.log('aspectAfterTest successful.');
